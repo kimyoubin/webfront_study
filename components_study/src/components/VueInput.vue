92ca9aade78da76d18da3bb2813892f5ae2c1e29
김유빈 <yb.kim@stickint.com>
2021-03-25 18:29:14 +0900

    input component 작업

--- components_study/src/components/VueInput.vue
+++ components_study/src/components/VueInput.vue
@@ -1,16 +1,41 @@
 <template>
-	<input type="text">
+	<input 
+		:type="type"
+		:placeholder="placeholder"
+		:name="name"
+		:id="id"
+		:value="value">
 </template>
 
 <script>
 export default {
-	name: '',
-	props: {
-		
+	name: 'VueInput',
+	props: [
+		'type', 'placeholder', 'name', 'id', 'value', 
+	],
+	data() {
+		return {
+			
+		}
 	}
 }
 </script>
 
 <style scoped>
+
+input {
+	width: 100%;
+	height: 50px;
+	padding: 10px;
+	margin-bottom: 15px;
+	font-size: 18px;
+	color: #333;
+	border: 1px solid #aeaeae;
+}
+
+input::placeholder {
+	font-size: 16px;
+	color: #aeaeae;
+}
 
 </style>
