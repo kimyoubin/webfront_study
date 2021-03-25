3198c171c66d5de2e57d9ce90db775ad81ed3928
김유빈 <yb.kim@stickint.com>
2021-03-25 18:29:24 +0900

    selectbox 컴포넌트 작업

---
+++ components_study/src/components/VueSelect.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="selectbox">
+        <button @click="toggleEvent">{{ selected }}</button>
+        <ul
+            v-if="isActive"
+            class="selectbox">
+            <!-- li 클릭시 item.option data가 button에 bind 돼야함 -->
+            <li 
+                v-for="(item, key) in items"
+                :key="key"
+                @click="selectOption(item)">{{ item.option }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+	name: 'VueSelect',
+	props: [
+        'items', 'selected'
+    ],
+	data() {
+		return {
+			isActive: false
+		}
+	},
+    methods: {
+        toggleEvent: function () {
+            
+            this.isActive = !this.isActive
+        },
+        selectOption: function(item) {
+            console.log(item);
+            console.log(this.selected);
+            item.option = this.selected
+        }
+    }
+}
+</script>
+
+<style scoped>
+.selectbox {
+    width: 100%;    
+}
+
+.selectbox button, .selectbox li {
+    width: 100%;
+    height: 50px;
+    padding: 10px;
+    font-size: 18px;
+    text-align: left;
+	color: #333;	
+}
+
+.selectbox button {
+    border: 1px solid #aeaeae;
+}
+
+.selectbox ul {
+
+}
+
+.selectbox ul li {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 50px;
+    border: 1px solid #aeaeae;
+    border-top: 0;
+    cursor: pointer;
+}
+
+</style>
--- components_study/src/views/StyleGuide.vue
+++ components_study/src/views/StyleGuide.vue
@@ -1,14 +1,76 @@
 <template>
-	<div class="StyleGuide">
-		<vue-input></vue-input>
-		스타일가이드
+	<div class="styleGuide">
+		<h1>StyleGuide</h1>
+		
+		<div class="wrap">
+			<h2>input text type</h2>
+			<vue-input 
+				type="text"
+				name="input-text"
+				id="input-text"
+				value=""
+				placeholder="ID를 입력해주세요."/>
+			<vue-input 
+				type="password"				
+				name="input-pw"
+				id="input-pw"
+				value=""
+				placeholder="PASSWORD를 입력해주세요."/>
+			<vue-input 
+				type="email"				
+				name="input-email"
+				id="input-email"
+				value=""
+				placeholder="E-mail을 입력해주세요."/>
+
+		</div>
+
+		<div class="wrap">
+			<h2>selectbox</h2>
+			<vue-select :items="listData"/>
+		</div>
 	</div>
 </template>
 
 <script>
 import VueInput from '../components/VueInput.vue'
+import VueSelect from '../components/VueSelect.vue'
 export default {
-    components: { VueInput },
+    components: { VueInput, VueSelect },
 	name: 'StyleGuide',
+	data() {
+		return {
+			listData: [
+				{
+					option: '선택하세요',
+				},
+				{
+					option: '선택하세요2',
+				},
+				{
+					option: '선택하세요3',
+				},
+				{
+					option: '선택하세요4',
+				}
+			]
+		}
+	}
 }
 </script>
+
+<style scoped>
+.styleGuide {
+	padding: 30px;
+	width: 500px;
+}
+.wrap {
+	margin-bottom: 30px;
+}
+h1 {
+	margin-bottom: 50px;
+}
+h2 {
+	margin-bottom: 10px;
+}
+</style>
